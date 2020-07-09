e41d5cf7dd05cb6e630b1eed5c7ab4c3aa5311ab
TXU95 <xutong5@icloud.com>
2020-07-09 23:23:39 +0800

    add a test file copy.py

---
+++ python/copy.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Solution():
+    '''
+    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    Example:
+
+        Given nums = [2, 7, 11, 15], target = 9,
+        Because nums[0] + nums[1] = 2 + 7 = 9,
+        return [0, 1].
+    '''
+
+    def twoSum(self, nums: list, target: int) -> list:
+        '''
+        :param nums: an array of integers
+        :param target: an integer
+        :return: indices of the two numbers such that they add up to a specific target
+        '''
+        dic = {}
+        for idx, num in enumerate(nums):
+            if num in dic:
+                return [dic[num], idx]
+            else:
+                dic[target - num] = idx
+
+
+if __name__ == '__main__':
+    nums = [2, 7, 11, 15]
+    target = 9
+    result = [0, 1]
+    solution = Solution()
+    answer = solution.twoSum(nums, target)
+    assert len(answer) == len(result), f'Your answer is {answer}, should be {result}'
+    assert all([True if answer[i] == result[i] else False
+                for i in range(len(result))]), f'Your answer is {answer}, should be {result}'
+    print('Pass test!')
+
